568096d7a93b75e3d34d4e5ab9e5a88b44952c98
NanoWar <robertkuhfss@gmail.com>
2017-09-30 20:28:11 +0200

    Moved credentials to env

---
+++ credentials.js
@@ -0,0 +1,12 @@
+var credentials = {};
+
+credentials.twitter = {
+    consumer_key: process.env.TWITTER_consumer_key,
+    consumer_secret: process.env.TWITTER_consumer_secret,
+    access_token_key: process.env.TWITTER_access_token_key,
+    access_token_secret: process.env.TWITTER_access_token_secret
+  };
+
+credentials.facebook = {};
+
+module.exports = credentials;
